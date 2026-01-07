587ced53c4fece5c103d2e83bb05909ae01e1292
choubeyshubham <shubhamchoubey96@gmail.com>
2026-01-07 10:53:16 +0530

    10 commit - Builder Design Pattern

---
+++ src/CreationalDesignPattern/Builder/Challange/Director.java
@@ -0,0 +1,12 @@
+package CreationalDesignPattern.Builder.Challange;
+
+class Director {
+    MealBuilderInterface myBuilder;
+
+    public void construct(MealBuilderInterface builder)
+    {
+        myBuilder = builder;
+        myBuilder.buildBurger();
+        myBuilder.buildDrink();
+    }
+}

20b45083bd28bedcd4211aed8b5183e3895f978c
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-19 12:43:19 +0530

    13 commit - Adapter Implmentation

---
+++ src/StructuralDesignPattern/Adaptor/Implementation/DuckExample/Duck.java
@@ -0,0 +1,16 @@
+package StructuralDesignPattern.Adaptor.Implementation.DuckExample;
+
+interface Duck {
+    public void quack();
+    public void fly();
+}
+
+class MallardDuck implements Duck {
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+    public void fly() {
+        System.out.println("I am flying");
+    }
+}
---
+++ src/StructuralDesignPattern/Adaptor/Implementation/DuckExample/Turkey.java
@@ -0,0 +1,16 @@
+package StructuralDesignPattern.Adaptor.Implementation.DuckExample;
+
+interface Turkey {
+    public void gobble();
+    public void fly();
+}
+
+class WildTurkey implements Turkey {
+    public void gobble() {
+        System.out.println("Gobble gobble");
+    }
+
+    public void fly() {
+        System.out.println("I am flying a short distance");
+    }
+}

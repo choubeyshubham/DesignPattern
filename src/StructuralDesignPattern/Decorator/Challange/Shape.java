a75a104aa27c6c2a63d1893d2fd4e6695234d4c6
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-21 06:31:10 +0530

    21 commit - Decorator Challange

---
+++ src/StructuralDesignPattern/Decorator/Challange/Shape.java
@@ -0,0 +1,21 @@
+package StructuralDesignPattern.Decorator.Challange;
+
+interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
+
+class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}

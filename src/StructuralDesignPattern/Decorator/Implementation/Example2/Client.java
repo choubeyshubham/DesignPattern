73d2e13d6486177ce9d400d89667575e7660d412
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-21 05:59:59 +0530

    20 commit - Decorator Implementation

---
+++ src/StructuralDesignPattern/Decorator/Implementation/Example2/Client.java
@@ -0,0 +1,26 @@
+package StructuralDesignPattern.Decorator.Implementation.Example2;
+
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args)
+    {
+        int c;
+
+        StringBuffer stringBuffer = new StringBuffer("Jason Fedin aaaBBBBcccDDDD");
+        byte[] bytes = stringBuffer.toString().getBytes();
+
+        try {
+            InputStream in = new LowerCaseInputStream(new ByteArrayInputStream(bytes));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+

e21263ab5835478ab521f8147d3866c7dac234f1
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-24 16:40:14 +0530

    32 commit - Interpretor Implementation

---
+++ src/BehavioralDesignPattern/Interpretor/Implementation/InterpreterContext.java
@@ -0,0 +1,12 @@
+package BehavioralDesignPattern.Interpretor.Implementation;
+
+class InterpreterContext {
+
+    public String getBinaryFormat(int i) {
+        return Integer.toBinaryString(i);
+    }
+
+    public String getHexadecimalFormat(int i) {
+        return Integer.toHexString(i);
+    }
+}

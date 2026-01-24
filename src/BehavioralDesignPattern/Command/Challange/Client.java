e8d018a9ead397e66c0ba14ca9fcbf8ad448fd1b
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-24 16:22:34 +0530

    31 commit - Command Challange

---
+++ src/BehavioralDesignPattern/Command/Challange/Client.java
@@ -0,0 +1,16 @@
+package BehavioralDesignPattern.Command.Challange;
+
+public class Client {
+    public static void main(String[] args) {
+        Stock googleStock = new Stock();
+
+        BuyStock buyStockOrder = new BuyStock(googleStock);
+        SellStock sellStockOrder = new SellStock(googleStock);
+
+        Broker broker = new Broker();
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(sellStockOrder);
+
+        broker.placeOrders();
+    }
+}

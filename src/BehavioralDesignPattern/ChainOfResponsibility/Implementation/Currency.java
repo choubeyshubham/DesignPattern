c1eeeac8b2590daa785102454cafe118ce663a79
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-24 10:30:07 +0530

    28 commit - Chain of Responsibiity implementation

---
+++ src/BehavioralDesignPattern/ChainOfResponsibility/Implementation/Currency.java
@@ -0,0 +1,13 @@
+package BehavioralDesignPattern.ChainOfResponsibility.Implementation;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amt) {
+        amount = amt;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}

24f0bff133f7f88dca1dda6f03b86d2fc0aebd8e
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-25 10:52:13 +0530

    34 commit - Iterator Implementation

---
+++ src/BehavioralDesignPattern/Iterator/Implementation/Client.java
@@ -0,0 +1,10 @@
+package BehavioralDesignPattern.Iterator.Implementation;
+
+public class Client {
+
+    public static void main(String [] args) {
+        NotificationCollection nc = new NotificationCollection();
+        NotificationBar nb = new NotificationBar(nc);
+        nb.printNotifications();
+    }
+}
---
+++ src/BehavioralDesignPattern/Iterator/Implementation/Iterator.java
@@ -0,0 +1,10 @@
+package BehavioralDesignPattern.Iterator.Implementation;
+
+interface Iterator {
+
+    // indicates wheter there are more elements to iterator over
+    boolean hasNext();
+
+    // returns the next element
+    Object next();
+}

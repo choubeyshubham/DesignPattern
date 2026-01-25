7aeaae4068a82cb5fe990b4927c7f4e40089b63e
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-25 23:22:10 +0530

    39 commit - Memento Challange

---
+++ src/BehavioralDesignPattern/Memento/Challange/Memento.java
@@ -0,0 +1,14 @@
+package BehavioralDesignPattern.Memento.Challange;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state)
+    {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}

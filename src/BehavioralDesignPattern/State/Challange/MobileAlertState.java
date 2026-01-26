0d680d17166e473bd71948a2cab4911c24e8fd70
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-26 08:25:21 +0530

    43 commit - State Challange

---
+++ src/BehavioralDesignPattern/State/Challange/MobileAlertState.java
@@ -0,0 +1,21 @@
+package BehavioralDesignPattern.State.Challange;
+
+interface MobileAlertState {
+    public void alert();
+}
+
+
+class Vibration implements MobileAlertState  {
+    @Override
+    public void alert()  {
+        System.out.println("vibration...");
+    }
+}
+
+class Silent implements MobileAlertState
+{
+    @Override
+    public void alert()   {
+        System.out.println("silent...");
+    }
+}

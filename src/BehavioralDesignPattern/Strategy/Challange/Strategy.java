b6b03bd9cb282d6c9984cb73b4c541a8091e44f0
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-01-26 10:13:29 +0530

    45 commit - Strategy Challange

---
+++ src/BehavioralDesignPattern/Strategy/Challange/Strategy.java
@@ -0,0 +1,27 @@
+package BehavioralDesignPattern.Strategy.Challange;
+
+interface Strategy {
+    public int performOperation(int num1, int num2);
+}
+
+class OperationAdd implements Strategy {
+    @Override
+    public int performOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+
+class OperationSubtract implements Strategy {
+    @Override
+    public int performOperation(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+class OperationMultiply implements Strategy {
+    @Override
+    public int performOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
